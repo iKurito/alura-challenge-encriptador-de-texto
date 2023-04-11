5d1c778df2c530c7215547ab5d7be241da121bde
Ronaldo <ronaldo.tunque@pucp.edu.pe>
2023-04-11 16:24:19 -0500

    update front view, styles and add copy function

--- script.js
+++ script.js
@@ -1,105 +1,35 @@
-const $encryptForm = document.getElementById("encryptForm");
-const $decryptForm = document.getElementById("decryptForm");
-const $encryptText = document.getElementById("encryptText");
-const $decryptText = document.getElementById("decryptText");
-const $encryptResult = document.getElementById("encryptResult");
-const $decryptResult = document.getElementById("decryptResult");
+// Author: Ronaldo Romario Tunque Cahui
+// Date: 11-04-2023
 
-const bufferABase64 = (buffer) => btoa(String.fromCharCode(...new Uint8Array(buffer)));
-const base64ABuffer = (buffer) => Uint8Array.from(atob(buffer), (c) => c.charCodeAt(0));
-const LONGITUD_SAL = 16;
-const LONGITUD_VECTOR_INICIALIZACION = LONGITUD_SAL;
+const $resultText = document.getElementById("resultText");
+const $encryptBtn = document.getElementById("encryptBtn");
+const $decryptBtn = document.getElementById("decryptBtn");
+const $copyBtn = document.getElementById("copyBtn");
 
-const keyBased = async (key, sal, iteraciones, longitud, hash, algoritmo = "AES-CBC") => {
-	const encoder = new TextEncoder();
-	let keyMaterial = await window.crypto.subtle.importKey(
-		"raw",
-		encoder.encode(key),
-		{ name: "PBKDF2" },
-		false,
-		["deriveKey"]
-	);
-	return await window.crypto.subtle.deriveKey(
-		{
-			name: "PBKDF2",
-			salt: encoder.encode(sal),
-			iterations: iteraciones,
-			hash,
-		},
-		keyMaterial,
-		{ name: algoritmo, length: longitud },
-		false,
-		["encrypt", "decrypt"]
-	);
-};
+function encrypt() {
+	console.log("encrypt");
+}
 
-const encriptar = async (key, textoPlano) => {
-	const encoder = new TextEncoder();
-	const sal = window.crypto.getRandomValues(new Uint8Array(LONGITUD_SAL));
-	const vectorInicializacion = window.crypto.getRandomValues(
-		new Uint8Array(LONGITUD_VECTOR_INICIALIZACION)
-	);
-	const bufferTextoPlano = encoder.encode(textoPlano);
-	const clave = await keyBased(key, sal, 100000, 256, "SHA-256");
-	const encrypted = await window.crypto.subtle.encrypt(
-		{ name: "AES-CBC", iv: vectorInicializacion },
-		clave,
-		bufferTextoPlano
-	);
-	return bufferABase64([...sal, ...vectorInicializacion, ...new Uint8Array(encrypted)]);
-};
+function decrypt() {
+	console.log("decrypt");
+}
 
-const encryptFunction = async ($event) => {
-	$event.preventDefault();
-	const key = "123456789";
-	if (!key) {
-		return alert("No hay key para encriptar");
-	}
-	const text = $encryptText.value;
-	if (!text) {
-		return alert("No hay texto para encriptar");
-	}
-	const encryptedText = await encriptar(key, text);
-	$encryptResult.value = encryptedText;
-};
+function copy() {
+	$resultText.select();
+	$resultText.setSelectionRange(0, 99999);
+	navigator.clipboard.writeText($resultText.value);
+}
 
-const desencriptar = async (key, encriptadoEnBase64) => {
-	const decoder = new TextDecoder();
-	const datosEncriptados = base64ABuffer(encriptadoEnBase64);
-	const sal = datosEncriptados.slice(0, LONGITUD_SAL);
-	const vectorInicializacion = datosEncriptados.slice(
-		0 + LONGITUD_SAL,
-		LONGITUD_SAL + LONGITUD_VECTOR_INICIALIZACION
-	);
-	const clave = await keyBased(key, sal, 100000, 256, "SHA-256");
-	const datosDesencriptadosComoBuffer = await window.crypto.subtle.decrypt(
-		{ name: "AES-CBC", iv: vectorInicializacion },
-		clave,
-		datosEncriptados.slice(LONGITUD_SAL + LONGITUD_VECTOR_INICIALIZACION)
-	);
-	return decoder.decode(datosDesencriptadosComoBuffer);
-};
+function setFunctions() {
+	$encryptBtn.onclick = encrypt;
+	$decryptBtn.onclick = decrypt;
+	$copyBtn.onclick = copy;
+}
 
-const decryptFunction = async ($event) => {
-	$event.preventDefault();
-	const password = "123456789";
-	if (!password) {
-		return alert("No hay contraseña");
-	}
-	const textoCifradoBase64 = $decryptText.value;
-	if (!textoCifradoBase64) {
-		return alert("No hay texto en base64");
-	}
-	try {
-		const desencriptado = await desencriptar(password, textoCifradoBase64);
-		$decryptResult.value = desencriptado;
-	} catch (e) {
-		$decryptResult.value =
-			"Error desencriptando: " +
-			e.message +
-			". ¿La clave es la correcta y la información está en base64?";
-	}
-};
+setFunctions();
 
-$encryptForm.onsubmit = encryptFunction;
-$decryptForm.onsubmit = decryptFunction;
+const $foundContainer = document.getElementById("foundContainer");
+const $notFoundContainer = document.getElementById("notFoundContainer");
+
+$foundContainer.style.display = "flex";
+$notFoundContainer.style.display = "none";

c1a22092e8b1e7574aeac99fb4abc19042724fa0
Ronaldo <ronaldo.tunque@pucp.edu.pe>
2023-04-11 17:43:18 -0500

    update front view, add encrypt and decrypt functions

--- script.js
+++ script.js
@@ -1,17 +1,70 @@
 // Author: Ronaldo Romario Tunque Cahui
 // Date: 11-04-2023
 
+const $text = document.getElementById("text");
 const $resultText = document.getElementById("resultText");
 const $encryptBtn = document.getElementById("encryptBtn");
 const $decryptBtn = document.getElementById("decryptBtn");
 const $copyBtn = document.getElementById("copyBtn");
+const $foundContainer = document.getElementById("foundContainer");
+const $notFoundContainer = document.getElementById("notFoundContainer");
+const $messageTittle = document.getElementById("messageTittle");
+
+const encryptValues = {
+	a: "ai",
+	e: "enter",
+	i: "imes",
+	o: "ober",
+	u: "ufat",
+};
+
+const decryptValues = {
+	ai: "a",
+	enter: "e",
+	imes: "i",
+	ober: "o",
+	ufat: "u",
+};
+
+function validate(text) {
+	// Regex para solo letras minúsculas y sin acentos
+	const re = RegExp(/^([a-z-!\s]+)$/g);
+	if (text.trim() === "") {
+		$foundContainer.style.display = "none";
+		$notFoundContainer.style.display = "flex";
+		$messageTittle.innerText = "Ningún mensaje fue encontrado";
+		return false;
+	}
+	const isValid = re.test(text);
+	if (!isValid) {
+		$foundContainer.style.display = "none";
+		$notFoundContainer.style.display = "flex";
+		$messageTittle.innerText = "Solo se permiten letras minúsculas y sin acentos";
+		return false;
+	}
+	$foundContainer.style.display = "flex";
+	$notFoundContainer.style.display = "none";
+	return true;
+}
 
 function encrypt() {
-	console.log("encrypt");
+	const textValue = $text.value;
+	let encryptTextValue = "";
+	const wasValidated = validate(textValue);
+	if (wasValidated) {
+		encryptTextValue = textValue.replaceAll(/[aeiou]/g, (x) => encryptValues[x]);
+	}
+	$resultText.value = encryptTextValue;
 }
 
 function decrypt() {
-	console.log("decrypt");
+	const textValue = $text.value;
+	let decryptTextValue = "";
+	const wasValidated = validate(textValue);
+	if (wasValidated) {
+		decryptTextValue = textValue.replaceAll(/ai|enter|imes|ober|ufat/g, (x) => decryptValues[x]);
+	}
+	$resultText.value = decryptTextValue;
 }
 
 function copy() {
@@ -27,9 +80,3 @@ function setFunctions() {
 }
 
 setFunctions();
-
-const $foundContainer = document.getElementById("foundContainer");
-const $notFoundContainer = document.getElementById("notFoundContainer");
-
-$foundContainer.style.display = "flex";
-$notFoundContainer.style.display = "none";
